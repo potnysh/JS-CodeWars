76d55fcf262beaa2b04747218865a462ffb6aec5
potnysh <110618245+potnysh@users.noreply.github.com>
2022-09-19 14:34:10 +0300

    codewars

---
+++ CodeWars.js
@@ -0,0 +1,7 @@
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+function findAverage(array) {  
+    return array.length > 0  
+      ? array.reduce((acc, el) => acc + el)/array.length
+      : 0
+    }
